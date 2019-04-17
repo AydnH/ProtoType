5e311eda7b30b416803557583fd534a4100ffc82
AydnH <37090813+AydnH@users.noreply.github.com>
2019-04-17 15:04:08 +1200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const debug = require('debug')('ga-pwn');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+  debug(`Example app listening on port ${port}!`);
+});
