eb8cb67aef0f905148490cfb3606d690ace088c5
AydnH <aydnhooper1997@gmail.com>
2019-05-01 14:25:30 +1200

    node modules and code update recovery from encrytion disaster

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
+
 const express = require('express');
-const debug = require('debug')('ga-pwn');
+const debug = require('debug')('ProtoType');
 const path = require('path');
+
 
 const app = express();
 const port = process.env.PORT || 8000;
 
+var bodyParser=require("body-parser");
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.post('/createEmp', function(req, res){  
+console.log(req.body);
+}); 
+
+app.get('/createEmp', (req, res) => {
+	res.sendFile(path.join(__dirname, 'index.html'));
+console.log();
 });
 
 app.listen(port, () => {
   debug(`Example app listening on port ${port}!`);
 
 });
+console.log('debug'. debug + console.log());
